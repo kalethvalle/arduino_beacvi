6c9f29ed8403059f1f0ccdd9b6cdfa179a70d6d4
kalethvalle <vallekaleth@gmail.com>
2021-05-26 20:20:21 -0500

    se actualiza para ejecutar los audios

--- other.py
+++ other.py
@@ -1,46 +1,50 @@
+import asyncio
 import serial, time
 from pathlib import Path
 from pygame import mixer  # Load the popular external library
 
-# variables globales
-path = './audio/'
-uid_compare = ''
+async def Home():
+   path = './audio/'
+   uid_compare = ''
+   # se establece conexion con el monitor serial de arduino
+   ser = serial.Serial('COM5', baudrate=9600, timeout=1)
+   print('Established serial connection to Arduino')
 
-# se establece conexion con el monitor serial de arduino
-ser = serial.Serial('COM5', baudrate=9600, timeout=1)
-print('Established serial connection to Arduino')
+   while True:
+      # ser.write(b"5")
+      arduino_data = ser.readline()
 
-while True:
-    # ser.write(b"5")
-    arduino_data = ser.readline()
+      decoded_values = str(arduino_data[0:len(arduino_data)].decode("utf-8"))
+      list_values = decoded_values.split(' ')
 
-    decoded_values = str(arduino_data[0:len(arduino_data)].decode("utf-8"))
-    list_values = decoded_values.split(' ')
+      uid = ''
+      for concat in list_values:
+         uid += concat
 
-    uid = ''
-    for concat in list_values:
-        uid += concat
+      if uid:
+         print(uid)
+      #    se valida si existe el mp3 para poder ser reproducido
+         file_true = Path(path + str(uid) + '.mp3').is_file()
 
-    if uid:
-       print(uid)
-    #    se valida si existe el mp3 para poder ser reproducido
-       file_true = Path(path + str(uid) + '.mp3').is_file()
+         mixer.init()
 
-       mixer.init()
+         if file_true:
+            mixer.music.load(path + str(uid) + '.mp3')
 
-       if file_true:
-          mixer.music.load(path + str(uid) + '.mp3')
+            if uid_compare != uid:
+               uid_compare = uid
+               mixer.music.play()
+            else:
+               uid_compare = ''
+               mixer.music.stop()
+         else:
+            uid_compare = ''
+            mixer.music.stop()
 
-          if uid_compare != uid:
-             uid_compare = uid
-             mixer.music.play()
-          else:
-             uid_compare = ''
-             mixer.music.stop()
-       else:
-           uid_compare = ''
-           mixer.music.stop()
+      time.sleep(0.01)
 
-    time.sleep(0.01)
-
-print('stop')
+async def main():
+    await Home()
+    
+if __name__ == "__main__":
+    asyncio.run(main())
